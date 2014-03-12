e094b12a7161fdc141a055fd83cf0d05d66f2c71
John Smith <info@whywouldwe.com>
2014-03-12 15:21:58 +0000

    add exec to ensure runit management commands work

--- run-mongo.sh
+++ run-mongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
 
-/srv/mongodb-linux-x86_64/bin/mongod --dbpath /data/mongodb --replSet rs0 --oplogSize 100 --rest --noprealloc --smallfiles 
+exec /srv/mongodb-linux-x86_64/bin/mongod --dbpath /data/mongodb --replSet rs0 --oplogSize 100 --rest --smallfiles
+
